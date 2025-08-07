fb7066b9bf9c60149326d5fda64299fddf66d182
nesuraisyn04 <nesuraisyn04@gmail.com>
2025-08-07 13:48:58 +0500

    feat(telegram): created send_message() and send_poll() in telegram.py.

---
+++ telegram_bot_polls/bot/telegram.py
@@ -0,0 +1,24 @@
+import requests
+
+TELEGRAM_TOKEN = "TOKEN"
+BASE_URL = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}"
+
+def send_message(chat_id, text):
+    url = f"{BASE_URL}/sendMessage"
+    payload = {"chat_id": chat_id, "text": text}
+    r = requests.post(url, json=payload)
+    return r
+
+def send_poll(chat_id, poll):
+    url = f"{BASE_URL}/getPoll"
+    payload = {
+        "chat_id": chat_id,
+        "question": poll.question,
+        "options": poll.options,
+        "is_anonymous": False
+    }
+    response = requests.post(url, json=payload).json()
+    if response.status_code == 200:
+        if "result" in response:
+            poll.poll_id = response["result"]["poll"]["id"]
+            poll.save()
