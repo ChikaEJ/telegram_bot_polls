5f7ec1f14a31d85a1bdb713c713c1651d7e2ae41
nesuraisyn04 <nesuraisyn04@gmail.com>
2025-08-07 13:52:20 +0500

    feat(views): created telegram_webhook()

--- telegram_bot_polls/bot/views.py
+++ telegram_bot_polls/bot/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+import json
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from telegram_bot_polls.user.models import UserProfileModel, PollModel
+from .telegram import send_poll, send_message
 
-# Create your views here.
+@csrf_exempt
+def telegram_webhook(request):
+    data = json.loads(request.body)
+
+    if "message" in data and "text" in data["message"]:
+        chat_id = data["message"]["chat"]["id"]
+        text = data["message"]["text"]
+
+        if text == "/start":
+            user, created = UserProfileModel.objects.get_or_create(telegram_id=chat_id)
+            send_message(chat_id, "Вы зарегистрированны!")
+
+        elif text == "/poll":
+            try:
+                poll = PollModel.objects.latest("created_at")
+                send_poll(chat_id, poll)
+            except PollModel.DoesNotExist:
+                send_message(chat_id, "Опросников нет!")
+
+    elif "poll_answer" in data:
+        answer = data["poll_answer"]
+        poll_id = answer["poll"]["id"]
+        user_chat_id = answer["user"]["id"]
+        selected = answer["options_ids"]
+
+        try:
+            user = UserProfileModel.objects.get(telegram_chat_id=user_chat_id)
+            poll = PollModel.objects.get(poll_id=poll_id)
+            user.completed_polls += 1
+            user.save()
+            poll.user_options.append(selected)
+            poll.save()
+        except Exception as e:
+            print(e)
+    return HttpResponse({"ok", True})
