4634b48c05000ca5794718d90e0af18da804933f
nesuraisyn04 <nesuraisyn04@gmail.com>
2025-08-07 12:50:14 +0500

    init(models): add UserProfileModel and PollModel

--- telegram_bot_polls/user/models.py
+++ telegram_bot_polls/user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    telegram_chat_id = models.BigIntegerField(unique=True)
+    completed_polls = models.PositiveIntegerField(default=0)
+
+class PollModel(models.Model):
+    user_profile = models.ForeignKey(
+        to='user.UserProfileModel',
+        on_delete=models.CASCADE,
+        related_name='user_polls',
+        db_index=True,
+    )
+    poll_id = models.BigIntegerField(null=True, blank=True)
+    question = models.TextField()
+    options = models.JSONField(default=list, null=True, blank=True)
+    user_options = models.JSONField(default=list, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
